cebef51d0e8eac891cc673d99311d1ee35fd6a18
Parminder Rattu <prattu@mail.sfsu.edu>
2020-09-04 13:03:44 -0700

    Initial Commit

---
+++ SFSU-Parminder-Rattu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SFSU-Parminder-Rattu
+//
+//  Created by Priya Rattu on 9/4/20.
+//  Copyright © 2020 SFSU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SFSU-Parminder-RattuTests/SFSU_Parminder_RattuTests.swift
@@ -0,0 +1,34 @@
+//
+//  SFSU_Parminder_RattuTests.swift
+//  SFSU-Parminder-RattuTests
+//
+//  Created by Priya Rattu on 9/4/20.
+//  Copyright © 2020 SFSU. All rights reserved.
+//
+
+import XCTest
+@testable import SFSU_Parminder_Rattu
+
+class SFSU_Parminder_RattuTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
